9da5bdaa668364963fdd15269cae5a28f7d7fb64
jaxtreme01 <jackmwa94@gmail.com>
2016-02-05 10:48:51 +0300

    fixed bugs final

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(name='scrible',
       entry_points={
           'console_scripts': ['scrible = scrible.notes.scrible:main'],
       },
-      packages=['scrible'],
-      install_requires=['docopt', 'clint', 'requests', 'python-firebase'
-                        ],
+      packages=['scrible','scrible/notes','scrible/sync','scrible/tests'],
+      install_requires=['docopt', 'clint', 'requests', 'python-firebase',
+                        'colorama', 'termcolor', 'pyfiglet'],
       include_package_data=True,
       zip_safe=False)
